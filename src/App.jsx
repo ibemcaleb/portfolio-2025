11f4725c7d9350966bacbd504a21c756e71f3abc
ibemcaleb <icaleb01@gmail.com>
2025-12-12 22:24:37 +0100

    Created all the components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 function App() {
 
   return (
     <>
-      <h1>Portfolio</h1>
+      <Header />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   )
 }
